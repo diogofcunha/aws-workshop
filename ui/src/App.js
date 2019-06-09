43ce17e9a0568440b9891fc2c2aee3de836578f5
Diogo Cunha <diogofncunha@gmail.com>
2019-06-09 12:18:59 +0100

    feat: 🎸 made App component render the asset grid

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getImages } from "./client";
+import AssetInfoOverlay from "./components/AssetInfoOverlay";
+import AssetGrid from "./components/AssetGrid";
 
 function App() {
-  return <div>Welcome</div>;
-}
+  const [assets, setAssets] = useState(undefined);
+  const [selectedAsset, setSelectedAsset] = useState(undefined);
 
+  useEffect(() => {
+    async function fetchImages() {
+      const images = await getImages();
+      setAssets(images || null);
+    }
+
+    fetchImages();
+  }, []);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      }}
+    >
+      {assets === undefined ? (
+        "Loading"
+      ) : assets && assets.length ? (
+        <div>
+          <AssetGrid assets={assets} setSelectedAsset={setSelectedAsset} />
+          {selectedAsset && (
+            <AssetInfoOverlay
+              id={selectedAsset}
+              onClose={() => setSelectedAsset(undefined)}
+            />
+          )}
+        </div>
+      ) : (
+        "No assets"
+      )}
+    </div>
+  );
+}
 export default App;
