af97c49eca6c25524c6519eb2187f99a3e9498ca
Diogo Cunha <diogofncunha@gmail.com>
2019-06-23 09:55:37 +0100

    feat: 🎸 added a plus icon

---
+++ ui/src/components/PlusIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function PlusIcon() {
+  return (
+    <svg viewBox="0 0 25 25">
+      <path
+        fill="white"
+        fillRule="evenodd"
+        d="M14.5 10.5h8.496c1.107 0 2.004.888 2.004 2 0 1.105-.89 2-2.004 2H14.5v8.496a1.997 1.997 0 0 1-2 2.004c-1.105 0-2-.89-2-2.004V14.5H2.004A1.997 1.997 0 0 1 0 12.5c0-1.105.89-2 2.004-2H10.5V2.004C10.5.897 11.388 0 12.5 0c1.105 0 2 .89 2 2.004V10.5z"
+      />
+    </svg>
+  );
+}
