e4e8770b5411559acfd1d4e78036b017b86283cb
Diogo Cunha <diogofncunha@gmail.com>
2019-06-30 17:10:34 +0100

    refactor: 💡 changed asset info according to new data shape

--- ui/src/components/AssetInfo.js
+++ ui/src/components/AssetInfo.js
@@ -8,23 +8,27 @@ const AssetInfo = ({ info }) => {
       <tbody>
         <tr>
           <td>Taken at:</td>
-          <td>{new Date(info.takenAt).toLocaleString()}</td>
+          <td>{new Date(info.addedAt).toLocaleString()}</td>
         </tr>
         <tr>
           <td>Resolution:</td>
-          <td>{info.resolution}</td>
+          <td>
+            {info.width}x{info.height}
+          </td>
         </tr>
+        {info.durationInSeconds > 0 && (
+          <tr>
+            <td>Duration:</td>
+            <td>{info.durationInSeconds} seconds</td>
+          </tr>
+        )}
         <tr>
           <td>Format:</td>
-          <td>{info.format}</td>
+          <td>{info.mimeType.split("/")[1]}</td>
         </tr>
         <tr>
           <td>Mime type:</td>
           <td>{info.mimeType}</td>
-        </tr>
-        <tr>
-          <td>Number of pixels:</td>
-          <td>{info.numberOfPixels}</td>
         </tr>
         <tr>
           <td>Orientation:</td>
