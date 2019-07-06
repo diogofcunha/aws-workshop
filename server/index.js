5d2053cbd4c0a3e39808ce666de1bf524eba6b38
Diogo Cunha <diogofncunha@gmail.com>
2019-07-06 18:43:24 +0100

    refactor: 💡 changed port for asset-api

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import Koa from "koa";
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = "Hello word";
+  ctx.body = "Hello world";
 });
 
-app.listen(3000);
+app.listen(5000);
 
-console.log("Listening at port 3000");
+console.log("Listening at port 5000");
 
 export default app;
