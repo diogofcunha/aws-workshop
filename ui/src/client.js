c744ce0ad98f9ee889bc7e73931278ee6d6a8a51
Diogo Cunha <diogofncunha@gmail.com>
2019-07-11 09:08:20 +0100

    feat: 🎸 added real code for save file client

--- ui/src/client.js
+++ ui/src/client.js
@@ -1,5 +1,35 @@
-export const saveFile = async () => {
-  return true;
+export const saveFile = async file => {
+  const res = await fetch(
+    `${
+      process.env.REACT_APP_API_ORIGIN
+    }/upload?content-type=${encodeURIComponent(file.type)}`,
+    {
+      method: "POST"
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to save file");
+  }
+
+  const presignedPost = await res.json();
+
+  const formData = new FormData();
+
+  Object.keys(presignedPost.fields).forEach(key => {
+    formData.append(key, presignedPost.fields[key]);
+  });
+
+  formData.append("Content-Type", file.type);
+  formData.append("file", file);
+  const s3Response = await fetch(presignedPost.url, {
+    method: "POST",
+    body: formData
+  });
+
+  if (!s3Response.ok) {
+    throw new Error("Failed to upload to Amazon S3");
+  }
 };
 
 export const getImages = async () => {
