b6aaf358adf1d4b03b4a16928c5e618ce0c4e2cb
Diogo Cunha <diogofncunha@gmail.com>
2019-07-11 12:01:21 +0100

    feat: 🎸 added util to extract s3 object key from sns event

---
+++ server/utils/s3Events.js
@@ -0,0 +1,24 @@
+export const getEventSourceObjectKey = event => {
+  // Read options from the event.
+  console.log(
+    "Reading options from event:\n",
+    util.inspect(event, { depth: 5 })
+  );
+
+  // Object key may have spaces or unicode non-ASCII characters.
+  const { Message } = JSON.parse(event.Records[0].body);
+
+  try {
+    const key = decodeURIComponent(
+      JSON.parse(Message).Records[0].s3.object.key.replace(/\+/g, " ")
+    );
+
+    if (!key) {
+      throw new Error();
+    }
+  } catch (ex) {
+    throw new Error(`Could not find key for event:`, Message);
+  }
+
+  return key;
+};
