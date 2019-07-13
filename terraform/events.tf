1f5bc3faa269773ce4b2ca7a06a2531d6d5b29b8
Diogo Cunha <diogofncunha@gmail.com>
2019-07-13 10:03:51 +0100

    feat: 🎸 added queue for asset info

--- terraform/events.tf
+++ terraform/events.tf
@@ -82,3 +82,49 @@ resource "aws_lambda_event_source_mapping" "create_thumbnail" {
   event_source_arn = "${aws_sqs_queue.thumbnail_queue.arn}"
   function_name    = "${aws_lambda_function.thumbnail.arn}"
 }
+
+resource "aws_sqs_queue" "asset_info_queue" {
+  name                      = "asset_info_queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "aws_sqs_queue_policy" "asset_info_queue_policy" {
+  queue_url = "${aws_sqs_queue.asset_info_queue.id}"
+
+  policy = <<JSON
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS":"*"},
+          "Action": "sqs:SendMessage",
+          "Resource": "${aws_sqs_queue.asset_info_queue.arn}",
+          "Condition": {
+            "ArnEquals": {
+              "aws:SourceArn": "${aws_sns_topic.bucket_event_topic.arn}"
+            }
+          }
+        },
+        {
+          "Effect": "Allow",
+          "Principal": {"AWS":"*"},
+          "Action": [
+            "sqs:GetQueueAttributes",
+            "sqs:GetQueueUrl",
+            "sqs:ReceiveMessage",
+            "sqs:DeleteMessage"
+          ],
+          "Resource": "${aws_sqs_queue.asset_info_queue.arn}"
+        }
+      ]
+    }
+    JSON
+}
