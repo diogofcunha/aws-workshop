8ab0d2eb9cd060b62352ac5faa909bcf84a531da
Diogo Cunha <diogofncunha@gmail.com>
2019-07-13 08:18:04 +0100

    feat: 🎸 using context to distribute variables to all resources
    this will help when using terraform modules that require a context about
    the aws account

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,14 @@
+variable "context" {
+  type = "map"
+
+  default = {
+    aws_profile    = "fullstack_try"
+    aws_account_id = "275679597069"
+    aws_region     = "eu-west-1"
+  }
+}
+
 provider "aws" {
-  region  = "eu-west-1"
-  profile = "fullstack_try"
+  profile = "${var.context["aws_profile"]}"
+  region  = "${var.context["aws_region"]}"
 }
