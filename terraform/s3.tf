45353403a9052316d5dee1a94365b072ebfac596
Diogo Cunha <diogofncunha@gmail.com>
2019-07-13 08:31:19 +0100

    feat: 🎸 added thumbnails s3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -15,3 +15,21 @@ resource "aws_s3_bucket" "asset_bucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket" "asset_thumbnails_bucket" {
+  bucket = "fullstack.diogo.workshop.thumbnails"
+  acl    = "private"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["POST"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
