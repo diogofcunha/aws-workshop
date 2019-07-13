a061bfaaa149bf16d0d6d9d292684a827b412e73
Diogo Cunha <diogofncunha@gmail.com>
2019-07-13 08:16:11 +0100

    feat: 🎸 added asset_api lambda

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "asset_bucket" {
   bucket = "fullstack.diogo.workshop"
   acl    = "private"
 
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["POST"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
